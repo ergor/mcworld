4c8b2312a9e4291e869058af320a92c0dcedbe58
Erol Gorancic <erolg@protonmail.com>
2019-09-09 17:57:05 +0200

    using JAI for reading

--- src/main/java/st/netb/mc/mcworld/Main.java
+++ src/main/java/st/netb/mc/mcworld/Main.java
@@ -1,45 +1,48 @@
 
 package st.netb.mc.mcworld;
 
+import com.sun.media.jai.codec.ByteArraySeekableStream;
+import com.sun.media.jai.codec.ImageCodec;
+import com.sun.media.jai.codec.SeekableStream;
 
-import org.apache.commons.imaging.ImageReadException;
-import org.apache.commons.imaging.Imaging;
-
-import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.awt.image.RenderedImage;
 import java.io.File;
-import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import com.sun.media.jai.codec.ImageDecoder;
 
 public class Main {
 
     public static void main(String[] args) {
 
         File dir = new File(args[0]);
-        List<File> tiffFiles = getAllTiffFiles(dir);
+        List<Path> tiffFiles = getAllTiffFiles(dir);
 
-
-        BufferedImage bufferedImage;
-
-        for (File file : tiffFiles) {
+        for (Path file : tiffFiles) {
             try {
-                bufferedImage = Imaging.getBufferedImage(file);
-                int value = bufferedImage.getRGB(0, 0);
-                int r = bufferedImage.getColorModel().getRed(value);
-                int g = bufferedImage.getColorModel().getGreen(value);
-                int b = bufferedImage.getColorModel().getBlue(value);
-                int a = bufferedImage.getColorModel().getAlpha(value);
-                int aa = bufferedImage.getColorModel().getAlpha(value);
-            } catch (ImageReadException | IOException e) {
+                SeekableStream stream = new ByteArraySeekableStream(Files.readAllBytes(file));
+                String[] names = ImageCodec.getDecoderNames(stream);
+                ImageDecoder dec = ImageCodec.createImageDecoder(names[0], stream, null);
+                RenderedImage im = dec.decodeAsRenderedImage();
+                Raster raster = im.getData();
+                float[] value = new float[1];
+                raster.getPixel(0, 0, value); // returns height in meters
+                System.out.println(value[0]);
+            }
+            catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private static List<File> getAllTiffFiles(File dir) {
+    private static List<Path> getAllTiffFiles(File dir) {
         return Arrays.stream(dir.listFiles())
                 .filter(p -> p.isFile() && p.getName().endsWith(".tif"))
+                .map(File::toPath)
                 .collect(Collectors.toList());
     }
 }
