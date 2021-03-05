e39cdfb494a923c37d027fd5bd75fe5a0ab35bff
Alexander Sagen <alexander.sagen@visma.com>
2021-03-05 16:50:15 +0100

    Removed Bukkit calls from MainRenderer to allow running from commandline

--- src/main/java/st/netb/mc/mcworld/MainRenderer.java
+++ src/main/java/st/netb/mc/mcworld/MainRenderer.java
@@ -1,6 +1,5 @@
 package st.netb.mc.mcworld;
 
-import org.bukkit.Bukkit;
 import st.netb.mc.mcworld.datasource.DataSource;
 import st.netb.mc.mcworld.datasource.FileType;
 import st.netb.mc.mcworld.datastructs.minecraft.coordinates.ChunkLocation;
@@ -32,11 +31,7 @@ public class MainRenderer {
     private File temporaryDir;
 
     public void init(String source, boolean fastRender) {
-        Bukkit.broadcastMessage("§6§l    DEBUG - 1");
-
         World world = getWorld(FileType.SOSI, new File(source).toPath());
-
-        Bukkit.broadcastMessage("§6§l    DEBUG - 2");
 
         GeoArea testArea = new GeoArea(
                 GeodeticDatum.EUREF89,
@@ -44,59 +39,37 @@ public class MainRenderer {
                 new Coordinate(597255, 6640726),
                 new Coordinate(601000, 6643455));
 
-        Bukkit.broadcastMessage("§6§l    DEBUG - 3");
-
         List<WorldSection> worldSections = world.getSections().stream()
         .filter(ws -> testArea.contains(ws.getArea()))
         .collect(Collectors.toList());
 
-        Bukkit.broadcastMessage("§6§l    DEBUG - 4");
-
         Map<ChunkLocation, ChunkBuilder> incompleteChunks = new HashMap<>();
-
-        Bukkit.broadcastMessage("§6§l    DEBUG - 5");
 
         temporaryDir = new File("tmp");
 
-        Bukkit.broadcastMessage("§6§l    DEBUG - 6");
-
         if (!fastRender) {
             IntermediateOutput ioWriter = new IntermediateOutput(temporaryDir);
-
-            Bukkit.broadcastMessage("§6§l    DEBUG - 7");
 
             for (WorldSection worldSection : worldSections) {
                 Tuple<List<ChunkBuilder>> chunkBuilders =
                         WorldMapper.toChunkBuilders(world, worldSection, incompleteChunks);
 
-                Bukkit.broadcastMessage("§6§l    DEBUG - 8");
-
                 List<ChunkBuilder> completeChunks = chunkBuilders.first();
                 List<ChunkBuilder> intersectingChunks = chunkBuilders.second();
-
-                Bukkit.broadcastMessage("§6§l    DEBUG - 9");
 
                 List<ChunkBuilder> updatedChunks = incompleteChunks.values().stream()
                         .filter(ChunkBuilder::isComplete)
                         .collect(Collectors.toList());
-
-                Bukkit.broadcastMessage("§6§l    DEBUG - 10");
 
                 updatedChunks.forEach(cs -> {
                     completeChunks.add(cs);
                     incompleteChunks.remove(cs.getChunkLocation());
                 });
 
-                Bukkit.broadcastMessage("§6§l    DEBUG - 11");
-
                 intersectingChunks.forEach(cs ->
                         incompleteChunks.put(cs.getChunkLocation(), cs));
 
-                Bukkit.broadcastMessage("§6§l    DEBUG - 12");
-
                 ioWriter.writeFiles(completeChunks);
-
-                Bukkit.broadcastMessage("§6§l    DEBUG - 13");
 
                 System.out.println("rendered " + completeChunks.size() + " chunks, " + intersectingChunks.size() + " on hold");
             }
