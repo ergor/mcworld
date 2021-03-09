9825cccfc6fc08c5e4da93a0e23ccc2d69a3d8e7
Alexander Sagen <alexander.sagen@visma.com>
2021-03-09 09:26:01 +0100

    Allow everything on spawn

--- src/main/java/st/netb/mc/mcworld/bukkit/WorldEventListener.java
+++ src/main/java/st/netb/mc/mcworld/bukkit/WorldEventListener.java
@@ -1,6 +1,8 @@
 package st.netb.mc.mcworld.bukkit;
 
+import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -19,6 +21,11 @@ public class WorldEventListener implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+        p.setGameMode(GameMode.CREATIVE);
+        p.setAllowFlight(true);
+        p.setFlying(true);
+        p.setOp(true);
         e.getPlayer().teleport(new Location(e.getPlayer().getWorld(), 8319, 108, 13403, -100, 51));
     }
 
