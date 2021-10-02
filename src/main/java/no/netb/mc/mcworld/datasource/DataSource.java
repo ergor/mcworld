b55fdc93a24ec530162eadb10aef6a458b60989d
erol <erol.gorancic@gmail.com>
2021-10-02 16:29:14 +0200

    Use javax.imageio instead of jai

--- src/main/java/no/netb/mc/mcworld/datasource/DataSource.java
+++ src/main/java/no/netb/mc/mcworld/datasource/DataSource.java
@@ -1,15 +1,11 @@
 package no.netb.mc.mcworld.datasource;
 
-import com.sun.media.jai.codec.ByteArraySeekableStream;
-import com.sun.media.jai.codec.ImageCodec;
-import com.sun.media.jai.codec.ImageDecoder;
-import com.sun.media.jai.codec.SeekableStream;
 import no.netb.mc.mcworld.datastructs.raw.WorldSection;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
-import java.awt.image.RenderedImage;
 import java.io.File;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -24,11 +20,8 @@ public interface DataSource {
 
     static Raster readImage(Path filePath) {
         try {
-            SeekableStream stream = new ByteArraySeekableStream(Files.readAllBytes(filePath));
-            String[] names = ImageCodec.getDecoderNames(stream);
-            ImageDecoder dec = ImageCodec.createImageDecoder(names[0], stream, null);
-            RenderedImage im = dec.decodeAsRenderedImage();
-            return im.getData(); // takes relatively long time (~1s for large files)
+            BufferedImage bufferedImage = ImageIO.read(filePath.toFile());
+            return bufferedImage.getData();
         }
         catch (Exception e) {
             throw new RuntimeException("Failed to read image " + filePath.toString() + ":", e);
